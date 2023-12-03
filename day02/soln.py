7f3f331ddf5bb8d3d49e91b29e203248f878866d
kaitshin <kshin@mit.edu>
2023-12-03 17:35:07 -0500

    day02 part2 soln

--- day02/soln.py
+++ day02/soln.py
@@ -4,7 +4,7 @@ import numpy as np
 with open('input.txt', 'r') as file:
     fline = file.readlines()
 
-# part 1
+## part 1
 def check_num_for_color_possible(line, color):
     """
     helper fn for `is_game_possible`
@@ -36,3 +36,28 @@ result = []
 for line in fline:
     result.append(is_game_possible(line))
 print(f"part 1: the sum is {sum(result)}")
+
+## part 2
+def get_max_num_per_color(line, color):
+    """
+    gets max num marbles shown for a given color
+    """
+    all_nums_per_color = re.findall(rf'\d+ {color}', line)
+    all_nums = np.array([int(result.split(' ')[0]) for result in all_nums_per_color])
+    return np.max(np.array(all_nums).astype(int))
+
+def get_power(line):
+    """
+    gets the power for a game
+    """
+    nums = []
+    for color in ['red', 'green', 'blue']:
+        nums.append(get_max_num_per_color(line, color))
+
+    power = nums[0]*nums[1]*nums[2]
+    return power
+
+result = []
+for line in fline:
+    result.append(get_power(line))
+print(f"part 2: the sum is {sum(result)}")
