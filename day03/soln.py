3199260aa9a5195c477a83ff43a8c7ffc53baa3a
kaitshin <kshin@mit.edu>
2023-12-04 18:00:55 -0500

    day03 part2 soln

--- day03/soln.py
+++ day03/soln.py
@@ -3,8 +3,11 @@ import numpy as np
 
 with open('input.txt', 'r') as file:
     fline = file.readlines()
+# split each line into separate characters
+char_matrix = np.array([np.array([char for char in line[:-1]]) for line in fline])
 N = len(fline)
 
+## part 1
 def check_edge_cases(line_idx, iis):
     """
     checks if the number is in the top row, bottom row, or has
@@ -75,14 +78,10 @@ def check_surrounding_idxs(line_idx, iis):
 
     return symbols
 
-
-# split each line into separate characters
-char_matrix = np.array([np.array([char for char in line[:-1]]) for line in fline])
-
 part_nums = []
 for line_idx, line in enumerate(fline):
     # find indexes of number in a line
-    num_iis = np.array([m for m in re.finditer(r'\d+', line)])
+    num_iis = [m for m in re.finditer(r'\d+', line)]
 
     for iis in num_iis:
         symbols = check_surrounding_idxs(line_idx, iis)
@@ -92,3 +91,41 @@ for line_idx, line in enumerate(fline):
         if len(unique_symbols) > 0:
             part_nums.append( int(line[iis.start():iis.end()]) )
 print(f"part 1: the sum is {sum(part_nums)}")
+
+
+## part 2
+gear_ratios = []
+for line_idx, line in enumerate(fline):
+    # find indexes of a * character
+    char_iis = [m for m in re.finditer(r'\*', line)]
+
+    for iis in char_iis:
+        first_line, last_line, first_char, last_char = check_edge_cases(line_idx, iis)
+        char_idx = iis.start()
+        gear_nums = []
+
+        if not first_line:
+            # check line above - is there a number whose indices are adj to the character index?
+            num_jjs = [m for m in re.finditer(r'\d+', fline[line_idx-1])]
+            for jj in num_jjs:
+                if char_idx >= jj.start()-1 and char_idx <= jj.end():
+                    gear_nums.append( int(fline[line_idx-1][jj.start():jj.end()]) )
+
+        if not last_line:
+            # check line below
+            num_jjs = [m for m in re.finditer(r'\d+', fline[line_idx+1])]
+            for jj in num_jjs:
+                if char_idx >= jj.start()-1 and char_idx <= jj.end():
+                    gear_nums.append( int(fline[line_idx+1][jj.start():jj.end()]) )
+
+        # also check the same line, to the left and right
+        num_jjs = [m for m in re.finditer(r'\d+', fline[line_idx])]
+        for jj in num_jjs:
+            if char_idx == jj.start()-1 or char_idx == jj.end():
+                gear_nums.append( int(fline[line_idx][jj.start():jj.end()]) )
+
+        # a gear is any * symbol that is adjacent to exactly two part numbers
+        if len(gear_nums) == 2:
+            gear_ratios.append(gear_nums[0]*gear_nums[1])
+
+print(f"part 2: the sum is {sum(gear_ratios)}")
